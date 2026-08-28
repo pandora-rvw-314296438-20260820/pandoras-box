9e8b27b1d4687cde7df4ac63719de80f69b5c5bb
pandora-rvw-314296438-20260820 <plpboracay@gmail.com>
2026-08-28 09:40:18 +0000

    chore(supabase): restore production cleanup migration 20260826051417

---
+++ supabase/migrations/20260826051417_remove_temporary_github_security_helpers_20260826.sql
@@ -0,0 +1,4 @@
+drop function if exists public.github_pat_safe_probe();
+drop function if exists public.github_security_baseline_apply();
+drop function if exists public.github_security_pr_create();
+drop function if exists public.github_security_state_probe();
