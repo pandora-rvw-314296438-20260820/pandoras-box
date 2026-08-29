c768fd9480a4edca7bf5a4f43ecafde958c725f6
pandora-rvw-314296438-20260820 <plpboracay@gmail.com>
2026-08-29 01:45:52 +0000

    feat(primitives): export Control Plane lineage serializers

--- packages/primitives/src/index.js
+++ packages/primitives/src/index.js
@@ -1,4 +1,4 @@
 'use strict';
-const contracts=require('./contracts'); const semver=require('./semver'); const {validateConfiguration}=require('./configuration'); const {TrustedPrimitiveRegistry,digest,primitiveKey,stableStringify}=require('./registry'); const {composePrimitives,deriveGeneratedSourceLineage}=require('./composition'); const {INITIAL_PRIMITIVES}=require('./catalog');
+const contracts=require('./contracts'); const semver=require('./semver'); const controlPlaneLineage=require('./control-plane-lineage'); const {validateConfiguration}=require('./configuration'); const {TrustedPrimitiveRegistry,digest,primitiveKey,stableStringify}=require('./registry'); const {composePrimitives,deriveGeneratedSourceLineage}=require('./composition'); const {INITIAL_PRIMITIVES}=require('./catalog');
 function createDefaultPrimitiveRegistry(){return new TrustedPrimitiveRegistry(INITIAL_PRIMITIVES);}
-module.exports={...contracts,...semver,INITIAL_PRIMITIVES,TrustedPrimitiveRegistry,composePrimitives,createDefaultPrimitiveRegistry,deriveGeneratedSourceLineage,digest,primitiveKey,stableStringify,validateConfiguration};
+module.exports={...contracts,...semver,...controlPlaneLineage,INITIAL_PRIMITIVES,TrustedPrimitiveRegistry,composePrimitives,createDefaultPrimitiveRegistry,deriveGeneratedSourceLineage,digest,primitiveKey,stableStringify,validateConfiguration};
