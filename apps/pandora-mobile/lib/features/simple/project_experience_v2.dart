0e9157c8466f5398d296cdb8d76b3623b04fafa3
pandora-rvw-314296438-20260820 <plpboracay@gmail.com>
2026-08-31 18:28:53 +0000

    style(mobile): preserve formatter spacing

--- apps/pandora-mobile/lib/features/simple/project_experience_v2.dart
+++ apps/pandora-mobile/lib/features/simple/project_experience_v2.dart
@@ -497,7 +497,6 @@ class _ProjectWorkspaceV2ScreenState extends State<ProjectWorkspaceV2Screen> {
     super.dispose();
   }
 
-
   Future<void> _startProjection() async {
     final repository =
         PandoraDependencies.of(context).projectExperienceRepository;
