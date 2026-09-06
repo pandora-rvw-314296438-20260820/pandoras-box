7eee4b0d57f4560e0956e162b72bb42c92d010dd
pandora-rvw-314296438-20260820 <plpboracay@gmail.com>
2026-09-06 07:48:59 +0000

    refactor: remove committed Control Tower mirror

--- public/control-tower/owner-screens.js
+++
@@ -1,7 +0,0 @@
-if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-window.PandorasOwnerScreens = Object.freeze({
-  ...window.PandorasOwnerScreenCore,
-  ...window.PandorasOwnerScreenActivity,
-  ...window.PandorasOwnerScreenMore,
-});
-}
