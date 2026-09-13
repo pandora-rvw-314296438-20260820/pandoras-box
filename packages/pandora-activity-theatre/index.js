e58b2170e549ad03b24f16b2599b32cb5002b5df
pandora-rvw-314296438-20260820 <plpboracay@gmail.com>
2026-09-13 16:44:48 +0000

    feat(activity-theatre): enforce Needs You real-boundary semantics

--- packages/pandora-activity-theatre/index.js
+++ packages/pandora-activity-theatre/index.js
@@ -3,4 +3,5 @@
 module.exports = Object.freeze({
   ...require("./src/activity-theatre-event.js"),
   ...require("./src/activity-truth-projection.js"),
+  ...require("./src/needs-you-boundary.js"),
 });
